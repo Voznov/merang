1784ca72ec64e8c61a1bf19e4cbf890d8643f41e
maximiloi <maximiloi@gmail.com>
2019-12-03 09:58:12 +0300

    переделал первую страницу полностью

--- React/src/app/DesktopApp.js
+++ React/src/app/DesktopApp.js
@@ -1,8 +1,5 @@
 import React, { Component } from 'react';
 import './DesktopApp.css'
-
-import phone from '../assets/phone.png';
-
 class DesktopApp extends Component {
     render() {
         return <div className="desktop-box">
@@ -12,7 +9,6 @@ class DesktopApp extends Component {
                 <h4 className="desktop-text-pattern desktop-text">Используйте мобильное устройство</h4>
                 {/* <h4 className="desktop-text-pattern desktop-text">Используйте мобильное устройство или скачайте <a href="https://merang.ru/downloads/">Android-приложение</a></h4> */}
             </div>
-            <img src={phone} className="desktop-img" alt="Наш сайт на телефоне" height="603px" />
         </div>
     }
 }
