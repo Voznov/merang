b68cb27ce75ce85a62114f76f5e64130f089b611
maximiloi <maximiloi@gmail.com>
2019-12-03 12:51:10 +0300

    добавлена структура для картинок в папке Assets

---
+++ React/src/pages/Slider.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+
+class Slider extends Component {
+  render() {
+    let slider = [
+      {
+        id: 1;
+        type: 'news',
+        background: '../assets/',
+        title: 'Приглашай друзей – получай бонусы!',
+        text: "1.jpg"
+      },
+    ]
+    return (
+      <div>
+
+      </div>
+    );
+  }
+}
+
+export default Slider;
